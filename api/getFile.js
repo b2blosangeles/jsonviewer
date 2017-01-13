ec5df8ab79d8a34e05fbcb5c7b8fdf2e2b8a3fa6
b2blosangeles <b2blosangeles@gmail.com>
2017-01-13 15:53:49 -0800

    Update getFile.js

--- api/getFile.js
+++ api/getFile.js
@@ -1,1 +1,15 @@
-res.send('hello');
+
+
+
+var opts = {
+  url: 'http://jsonviewer.qalet.com/README.md',
+  timeout: 60000
+}
+
+pkg.request(opts, function (err, res, body) {
+  if (err) {
+   res.send(err)
+    return
+  }
+  res.send(body);
+});
