6ceb496d7c051d6550e69683cfa0514dbb5ec96f
b2blosangeles <b2blosangeles@gmail.com>
2017-01-13 20:57:05 -0800

    test

--- api/getFile.js
+++ api/getFile.js
@@ -1,15 +1,13 @@
-
-var showdown  = require(env.space_path + '/api/pkg/showdown/node_modules/showdown'),
-    converter = new showdown.Converter(),
-    text      = '#hello, markdown!',
-    html      = converter.makeHtml(text);
-	res.send(html);
-/*
 pkg.request('http://jsonviewer.qalet.com/README.md', function (err, resp, body) {
+	
+	
+	
   if (err) {
    res.send(err)
     return
   }
-  res.send(body);
+  
+   var showdown  = require(env.space_path + '/api/pkg/showdown/node_modules/showdown'),
+    converter = new showdown.Converter();
+	res.send(converter.makeHtml(body));  
 });
-*/
