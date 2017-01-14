16386c19ba92a59b1b833d71b4900bc4dc4daba9
b2blosangeles <b2blosangeles@gmail.com>
2017-01-13 20:45:16 -0800

    test

--- api/getFile.js
+++ api/getFile.js
@@ -1,3 +1,10 @@
+res.send(env);
+/*
+var showdown  = require('showdown'),
+    converter = new showdown.Converter(),
+    text      = '#hello, markdown!',
+    html      = converter.makeHtml(text);
+
 pkg.request('http://jsonviewer.qalet.com/README.md', function (err, resp, body) {
   if (err) {
    res.send(err)
@@ -5,3 +12,4 @@ pkg.request('http://jsonviewer.qalet.com/README.md', function (err, resp, body) 
   }
   res.send(body);
 });
+*/
