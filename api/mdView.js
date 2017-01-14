d132ae7b3ae90007abaf894484b62da31d4e60d2
b2blosangeles <b2blosangeles@gmail.com>
2017-01-14 11:29:25 -0800

    test

---
+++ api/mdView.js
@@ -0,0 +1,15 @@
+pkg.request('http://d3.qalet.com/README.md', function (err, resp, body) {
+	
+	
+	
+  if (err) {
+   res.send(err)
+    return
+  }
+  /*
+   var showdown  = require(env.space_path + '/api/pkg/showdown/node_modules/showdown'),
+    converter = new showdown.Converter();
+	res.send(converter.makeHtml(body));  
+	*/
+	res.send(body)
+});
