ff62a8a0fb6e12d010387f68678dd9f955e782dc
b2blosangeles <b2blosangeles@gmail.com>
2017-01-14 18:36:03 -0800

    test

--- api/mdView.js
+++ api/mdView.js
@@ -2,19 +2,19 @@ var url = req.query.md;
 if (url) {
 	pkg.request(url, function (err, resp, body) {
 		
-		res.send(resp)
-		return true;
+		if (resp.statusCode != 200) {
+			res.send(url + ' unexist!');	
+			return true;
+		} else {
 		
-	  if (err) {
-	   res.send(err)
-		return
-	  }
-
-	   var showdown  = require(env.space_path + '/api/pkg/showdown/node_modules/showdown'),
-		converter = new showdown.Converter();
-		res.send(converter.makeHtml(body));  
-		
-		//res.send(body)
+			if (err) {
+				res.send(err)
+				return true;
+			}			
+			var showdown  = require(env.space_path + '/api/pkg/showdown/node_modules/showdown'),
+			converter = new showdown.Converter();
+			res.send(converter.makeHtml(body)); 
+		}
 	});
 } else {
 	 res.send('Missing md!');
