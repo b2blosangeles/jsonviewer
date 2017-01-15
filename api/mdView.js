6b01449bd354cad66567568e472d583ef30e96b3
b2blosangeles <b2blosangeles@gmail.com>
2017-01-14 18:17:04 -0800

    test

--- api/mdView.js
+++ api/mdView.js
@@ -1,15 +1,17 @@
 var url = req.query.md;
-res.send(url);
-return true;
-pkg.request('http://d3.qalet.com/README.md', function (err, resp, body) {
-  if (err) {
-   res.send(err)
-    return
-  }
+if (url) {
+	pkg.request(url, function (err, resp, body) {
+	  if (err) {
+	   res.send(err)
+		return
+	  }
 
-   var showdown  = require(env.space_path + '/api/pkg/showdown/node_modules/showdown'),
-    converter = new showdown.Converter();
-	res.send(converter.makeHtml(body));  
-	
-	//res.send(body)
-});
+	   var showdown  = require(env.space_path + '/api/pkg/showdown/node_modules/showdown'),
+		converter = new showdown.Converter();
+		res.send(converter.makeHtml(body));  
+		
+		//res.send(body)
+	});
+} else {
+	 res.send('Missing md!');
+}
