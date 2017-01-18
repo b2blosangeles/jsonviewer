e445323e3c029147005739218ee2ad8c3182d7e7
b2blosangeles <b2blosangeles@gmail.com>
2017-01-17 16:43:27 -0800

    Update HtmlViewer.js

--- package/HtmlViewer.js
+++ package/HtmlViewer.js
@@ -3,6 +3,7 @@
       		"/js/jquery-2.1.3.min.js",  
          	"/js/jquery.json-viewer.js",
 		"/js/showdown/showdown.js",
-		"/js/x2js/xml2json.js"
+		"/js/x2js/xml2json.js",
+		"/js/json2.min.js"
         ]
 }
