90768df4b92e3b848a98d44c222bf1aa148008a7
b2blosangeles <b2blosangeles@gmail.com>
2017-01-20 12:15:04 -0800

    Update react.js

--- package/react.js
+++ package/react.js
@@ -2,6 +2,7 @@
     "tpls":[],
     "filelist":[
         "/js/react/browser.min.js",  
-        "/js/react/react.min.js"
+        "/js/react/react.min.js",
+        "/js/react/react-dom.min.js"
       ]
 }
