e1edad3f6c0eca2952a88346d8dc29d9acc92f79
b2blosangeles <b2blosangeles@gmail.com>
2017-01-22 14:05:29 -0800

    test

--- jsx/a1.jsx
+++ jsx/a1.jsx
@@ -1,14 +1,3 @@
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(
-    element,
-    document.getElementById('example')
-  );
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
-
-setInterval(tick, 1000);
