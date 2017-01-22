dc990cbeacc17ece0979a93e093921da96fdf7ef
b2blosangeles <b2blosangeles@gmail.com>
2017-01-21 23:14:03 -0800

    test

--- api/jsxChannel.js
+++ api/jsxChannel.js
@@ -1,13 +1,13 @@
-//var Babel  = require(env.root_path + "/package/qaletBabel/qaletBabel.js");
-//var qaletBabel = new Babel();
+var Babel  = require(env.root_path + "/package/qaletBabel/qaletBabel.js");
+var qaletBabel = new Babel();
 
-				var b = require(env.root_path + "/package/qaletBabel/node_modules/babel-core").transform("code\neee", {
-				  plugins: [env.root_path + '/package/qaletBabel/node_modules/babel-plugin-transform-react-jsx']
+	//			var b = require(env.root_path + "/package/qaletBabel/node_modules/babel-core").transform("code\neee", {
+	//			  plugins: [env.root_path + '/package/qaletBabel/node_modules/babel-plugin-transform-react-jsx']
 				});
-res.send(b.code);
+//res.send(b.code);
 
-//var k = qaletBabel.jsx2jsCode(env.space_path+'/jsx/code/a1.jsx', function() {} );
-//res.send(k);
+var k = qaletBabel.jsx2jsCode(env.space_path+'/jsx/code/a1.jsx');
+res.send(k);
 /*
 qaletBabel.jsx2jsCode(env.space_path+'/jsx/code/a1.jsx', function(err, v) {
 	if (err) {
