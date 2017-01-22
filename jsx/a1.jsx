21dcccaf89134ab1c54be68df47359db2d5333e6
b2blosangeles <b2blosangeles@gmail.com>
2017-01-22 15:15:57 -0800

    test

--- jsx/a1.jsx
+++ jsx/a1.jsx
@@ -39,3 +39,22 @@ ReactDOM.render(
     author={comment.author} />,
   document.getElementById('example')
 );
+
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li>{number}</li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+const numbers = [1, 2, 3, 4, 5];
+ReactDOM.render(
+  <NumberList numbers={numbers} />,
+  document.getElementById('example3')
+);
+
+
