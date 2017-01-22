c5fc71d332000dd411611d97e018af8821a492ce
b2blosangeles <b2blosangeles@gmail.com>
2017-01-21 23:14:43 -0800

    test

--- api/jsxChannel.js
+++ api/jsxChannel.js
@@ -3,7 +3,7 @@ var qaletBabel = new Babel();
 
 	//			var b = require(env.root_path + "/package/qaletBabel/node_modules/babel-core").transform("code\neee", {
 	//			  plugins: [env.root_path + '/package/qaletBabel/node_modules/babel-plugin-transform-react-jsx']
-				});
+		//		});
 //res.send(b.code);
 
 var k = qaletBabel.jsx2jsCode(env.space_path+'/jsx/code/a1.jsx');
