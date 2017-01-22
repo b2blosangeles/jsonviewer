565d75354381f8c47f6042d595a69d2b1fc40845
b2blosangeles <b2blosangeles@gmail.com>
2017-01-22 14:15:16 -0800

    test

--- jsx/a1.jsx
+++ jsx/a1.jsx
@@ -1,5 +1,7 @@
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello i, {this.props.name}</h1>;
+  }
 }
 ReactDOM.render(
   <Welcome name="Sara" />,
