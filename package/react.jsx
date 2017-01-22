6582feedead3798b04226787818528243cd287d5
b2blosangeles <b2blosangeles@gmail.com>
2017-01-22 00:13:50 -0800

    test

--- package/react.jsx
+++ package/react.jsx
@@ -2,7 +2,7 @@
     "tpls":[],
     "filelist":[
         "/js/react/react.min.js",
-        "/js/react/react-dom.min.js"	
+        "/js/react/react-dom.min.js",	
         "/jsx/a1.jsx",
         "/jsx/a2.jsx"
       ]
