7d0490a6d4ac6514ec8a11bfc1928a2920c2ca12
b2blosangeles <b2blosangeles@gmail.com>
2017-01-21 21:47:00 -0800

    test

--- jsx/code/a1.jsx
+++ jsx/code/a1.jsx
@@ -1,9 +1,4 @@
-setTimeout(
-	function() {
-		var myDivElement = 
-			<div>商务部：2017年将大幅放宽外资准入限制</div>
-			;
-		ReactDOM.render(myDivElement,document.getElementById('example'));	
-	}, 1000
-);
-
+var myDivElement = 
+	<div>商务部：2017年将大幅放宽外资准入限制</div>
+	;
+ReactDOM.render(myDivElement,document.getElementById('example'));	
