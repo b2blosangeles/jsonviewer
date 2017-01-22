69da4f9714655fe0710476f61f3351ba1c7f3e22
b2blosangeles <b2blosangeles@gmail.com>
2017-01-22 00:09:39 -0800

    test

--- package/react.jsx
+++ package/react.jsx
@@ -1,8 +1,8 @@
 {
     "tpls":[],
     "filelist":[
-        "/js/react/react.min.js",
-        "/js/react/react-dom.min.js",	
+        "/js/react/react.js",
+        "/js/react/react-dom.js",	
         "/jsx/a1.jsx",
         "/jsx/a2.jsx"
       ]
