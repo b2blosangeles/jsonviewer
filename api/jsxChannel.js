fe31147141924063acd344bfdb2288eb99aa6d43
b2blosangeles <b2blosangeles@gmail.com>
2017-01-21 22:31:01 -0800

    test

--- api/jsxChannel.js
+++ api/jsxChannel.js
@@ -1,7 +1,7 @@
 var Babel  = require(env.root_path + "/package/qaletBabel/qaletBabel.js");
 var qaletBabel = new Babel();
 
-qaletBabel.jsx2js(env.space_path+'/jsx/code/a1.jsx', function(err, v) {
+qaletBabel.jsx2jsCode(env.space_path+'/jsx/code/a1.jsx', function(err, v) {
 	if (err) {
 		res.send(err.message);
 	} else {
