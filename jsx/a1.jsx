ebf59407e23bbc09efe485082a3f202baefe9d0e
b2blosangeles <b2blosangeles@gmail.com>
2017-01-22 14:22:07 -0800

    test

--- jsx/a1.jsx
+++ jsx/a1.jsx
@@ -1,9 +1,35 @@
-class Welcome extends React.Component {
-  render() {
-    return <h1>Hello i, {this.props.name}</h1>;
-  }
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
 }
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+};
+
 ReactDOM.render(
-  <Welcome name="Sara" />,
+  comment,
   document.getElementById('example')
 );
