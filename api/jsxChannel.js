29f4c1a8d66d512b05ca883a599348847e65cf19
b2blosangeles <b2blosangeles@gmail.com>
2017-01-21 21:32:29 -0800

    test

--- api/jsxChannel.js
+++ api/jsxChannel.js
@@ -1,6 +1,7 @@
-var pp  = require(env.space_path + "/api/pkg/qaletBabel/qaletBabel.js");
-var q = new pp();
-q.jsx2js(env.space_path+'/jsx/code/a1.jsx', function(err, v) {
+var Babel  = require(env.space_path + "/api/pkg/qaletBabel/qaletBabel.js");
+var qaletBabel = new Babel();
+
+qaletBabel.jsx2js(env.space_path+'/jsx/code/a2.jsx', function(err, v) {
 	if (err) {
 		res.send(err.message);
 	} else {
