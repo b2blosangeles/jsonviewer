c1474569fa1f91ad545c35a776cd7f7ed31a2239
b2blosangeles <b2blosangeles@gmail.com>
2017-01-22 10:00:59 -0800

    test

--- jsx/a1.jsx
+++ jsx/a1.jsx
@@ -2043,6 +2043,6 @@ var myDivElement =
 	;
 ReactDOM.render(myDivElement,document.getElementById('example'));	
 var myDivElement = 
-	<div><h1>因为这张照片 网友请愿取消她的选美冠军头衔kkksss</h1></div>
+	<div><h1>因为这张照片 网友请愿取消她的选美冠军头衔</h1></div>
 	;
 ReactDOM.render(myDivElement,document.getElementById('example'));	
