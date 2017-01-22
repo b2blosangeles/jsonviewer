eb31e0396790bd47b087545154d7f2a504ca8131
b2blosangeles <b2blosangeles@gmail.com>
2017-01-21 23:07:52 -0800

    test

--- api/jsxChannel.js
+++ api/jsxChannel.js
@@ -1,9 +1,9 @@
 //var Babel  = require(env.root_path + "/package/qaletBabel/qaletBabel.js");
 //var qaletBabel = new Babel();
-
+/*
 				var b = require(env.root_path + "/package/qaletBabel/node_modules/babel-core").transform('code', {
 				  plugins: [env.root_path + '/package/qaletBabel/node_modules/babel-plugin-transform-react-jsx']
-				});
+				});*/
 res.send('vv');
 
 //var k = qaletBabel.jsx2jsCode(env.space_path+'/jsx/code/a1.jsx', function() {} );
