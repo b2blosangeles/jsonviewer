ce9e60d23052e706428948f96267fc4f10051ce0
b2blosangeles <b2blosangeles@gmail.com>
2017-01-23 20:25:41 -0800

    test

--- jsx/a2.jsx
+++ jsx/a2.jsx
@@ -43,13 +43,6 @@ var AJAX = React.createClass({
 });
 
 ReactDOM.render(
-  <AJAX url="http://docviewer.qalet.com//sample/data.json" <!--promise={$.ajax(
-		{
-			url:'http://docviewer.qalet.com//sample/data.json',
-			method: "POST",
-			dataType: 'json',
-			cache: false, 
-			data:{}
-		})}--> />,
+  <AJAX url="http://docviewer.qalet.com//sample/data.json"  />,
   $('#example2')[0]
 );
