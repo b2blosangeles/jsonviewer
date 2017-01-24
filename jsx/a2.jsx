511f1418c5284308d4d6e1b199493353d9d78df1
b2blosangeles <b2blosangeles@gmail.com>
2017-01-24 12:33:04 -0800

    Update a2.jsx

--- jsx/a2.jsx
+++ jsx/a2.jsx
@@ -62,7 +62,7 @@ var AJAX = React.createClass({
 	if (!this.state.data) {
 		return false;
 	}
-      var repos =  this.state.data.items;
+      var repos =  (this.state.data)?this.state.data.items:[]; 
       var repoList = repos.map(function (repo, index) {
         return (
           <li key={index}><a href={repo.html_url}>{repo.name}</a> ->>-<Cc t={repo}></Cc> --  ({repo.stargazers_count} stars) <br/> {repo.description}</li>
