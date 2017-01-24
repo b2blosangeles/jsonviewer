3408603844799e663375fe4edf78ef172107a6af
b2blosangeles <b2blosangeles@gmail.com>
2017-01-24 07:12:03 -0800

    test

--- jsx/a2.jsx
+++ jsx/a2.jsx
@@ -20,7 +20,7 @@ var AJAX = React.createClass({
 	console.log('--componentWillReceiveProps--');
   },  
   
-  shouldComponentUpdate()() {
+  shouldComponentUpdate() {
 	console.log('--shouldComponentUpdate--');
   }, 
   
