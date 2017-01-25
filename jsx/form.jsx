d4f15743dffc1177809b5668d7b0d3e02040985e
b2blosangeles <b2blosangeles@gmail.com>
2017-01-25 14:08:22 -0800

    Update form.jsx

--- jsx/form.jsx
+++ jsx/form.jsx
@@ -1,18 +1,18 @@
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {form: {}};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({form:{value: event.target.value}});
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    alert('A name was submitted: ' + this.state.form.value);
     event.preventDefault();
   }
 
@@ -21,9 +21,9 @@ class NameForm extends React.Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="text" value={this.state.form.value} onChange={this.handleChange} />
         </label>
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Submit==" />
       </form>
     );
   }
