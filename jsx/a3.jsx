de93487b3f9576041fae9097d51279644fb2afe1
b2blosangeles <b2blosangeles@gmail.com>
2017-01-25 12:23:30 -0800

    Update a3.jsx

--- jsx/a3.jsx
+++ jsx/a3.jsx
@@ -62,7 +62,7 @@ class AJAX1 extends React.Component {
       var repos =  this.state.data.items;
       var repoList = repos.map(function (repo, index) {
         return (
-          <li key={index}><a href={repo.html_url}>{repo.name}</a> ->>-<Cc t={repo}></Cc> --  ({repo.stargazers_count} stars) <br/> {repo.description}</li>
+          <li key={index}><a href={repo.html_url}>{repo.name}</a> -^^-<Cc t={repo}></Cc> --  ({repo.stargazers_count} stars) <br/> {repo.description}</li>
         );
       }); 
 	}  
