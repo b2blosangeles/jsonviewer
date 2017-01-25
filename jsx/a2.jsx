4b503d1421126c55eabf7dffa6a40f4af15e59af
b2blosangeles <b2blosangeles@gmail.com>
2017-01-24 17:20:57 -0800

    test

--- jsx/a2.jsx
+++ jsx/a2.jsx
@@ -50,12 +50,26 @@ class AJAX extends React.Component {
   }
   
   render() {
+     if (this.state.loading) {
+      return <span>Loading...</span>;
+    }
+    else  if (this.state.error !== null) {
+      return <span>Error--: {this.state.error.message}</span>;
+    }
+    else {
+      var repos =  this.state.data.items;
+      var repoList = repos.map(function (repo, index) {
+        return (
+          <li key={index}><a href={repo.html_url}>{repo.name}</a> ->>-<Cc t={repo}></Cc> --  ({repo.stargazers_count} stars) <br/> {repo.description}</li>
+        );
+      }); 
+	}
       return (
         <span>
           niu
         </span>
       );
-    }
+	}
 };
 
 ReactDOM.render(
