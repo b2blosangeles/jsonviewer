e482923f051eee4016b341fbb35544cb9bf82ac4
b2blosangeles <b2blosangeles@gmail.com>
2017-01-25 14:00:35 -0800

    Update react_app.jsx

--- package/react_app.jsx
+++ package/react_app.jsx
@@ -4,9 +4,6 @@
         "/js/react/react.min.js",
         "/js/react/react-dom.min.js",
 		"/js/jquery-2.1.3.min.js",
-        "/jsx/a1.jsx",
-		"/jsx/a3.jsx",
-	    	"/jsx/a2.jsx",
 	    	"/jsx/form.jsx"
       ]
 }
