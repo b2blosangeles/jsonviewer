69f76cbf80180fb96eaa41747baf87f74b0b1b04
b2blosangeles <b2blosangeles@gmail.com>
2017-01-25 18:30:54 -0800

    test

--- jsx/router.jsx
+++ jsx/router.jsx
@@ -54,7 +54,6 @@ class Contact extends React.Component {
 ReactDOM.render((
    <Router history = {browserHistory}>
       <Route path = "/" component = {App}>
-         <IndexRoute component = {Home} />
          <Route path = "home" component = {Home} />
          <Route path = "about" component = {About} />
          <Route path = "contact" component = {Contact} />
