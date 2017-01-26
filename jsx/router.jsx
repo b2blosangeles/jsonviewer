8845a74babff0a6225fdcfc4b054132efe54b72e
b2blosangeles <b2blosangeles@gmail.com>
2017-01-25 18:45:00 -0800

    test

--- jsx/router.jsx
+++ jsx/router.jsx
@@ -3,59 +3,10 @@ let RouteHandler = Router.RouteHandler;
 let Route = Router.Route;
 let DefaultRoute = Router.DefaultRoute;
 
-class App extends React.Component {
-   render() {
-      return (
-         <div>
-            <ul>
-               <li>Home</li>
-               <li>About</li>
-               <li>Contact</li>
-            </ul>
-				
-           {this.props.children}
-         </div>
-      )
-   }
-}
+<Router history={history}>
+    <Route path="/" component={MainComponent}>
+      //<Route path="topics/:id" compponent={Topic} />
+    </Route>
+  </Router>
 
-class Home extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>Home...</h1>
-         </div>
-      )
-   }
-}
-
-class About extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>About...</h1>
-         </div>
-      )
-   }
-}
-
-class Contact extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>Contact...</h1>
-         </div>
-      )
-   }
-}
-
-ReactDOM.render((
-   <Router history = {browserHistory}>
-      <Route path = "/" component = {App}>
-         <Route path = "home" component = {Home} />
-         <Route path = "about" component = {About} />
-         <Route path = "contact" component = {Contact} />
-      </Route>
-   </Router>
-	
-), document.getElementById('app'))
+  ReactDOM.render(<Router />,  document.getElementById('app'));
