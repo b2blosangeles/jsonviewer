29be30547c43c154cdcaf00dceb8bd04caef9973
b2blosangeles <b2blosangeles@gmail.com>
2017-01-25 17:55:27 -0800

    test

--- jsx/router.jsx
+++ jsx/router.jsx
@@ -1,11 +1,63 @@
-render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <Route path="about" component={About}/>
-      <Route path="users" component={Users}>
-        <Route path="/user/:userId" component={User}/>
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            <ul>
+               <li>Home</li>
+               <li>About</li>
+               <li>Contact</li>
+            </ul>
+				
+           {this.props.children}
+         </div>
+      )
+   }
+}
+
+export default App;
+
+class Home extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Home...</h1>
+         </div>
+      )
+   }
+}
+
+export default Home;
+
+class About extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>About...</h1>
+         </div>
+      )
+   }
+}
+
+export default About;
+
+class Contact extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Contact...</h1>
+         </div>
+      )
+   }
+}
+
+ReactDOM.render((
+   <Router history = {browserHistory}>
+      <Route path = "/" component = {App}>
+         <IndexRoute component = {Home} />
+         <Route path = "home" component = {Home} />
+         <Route path = "about" component = {About} />
+         <Route path = "contact" component = {Contact} />
       </Route>
-      <Route path="*" component={NoMatch}/>
-    </Route>
-  </Router>
-), document.getElementById('root'))
+   </Router>
+	
+), document.getElementById('app'))
