42824e1fdc12df91826a86351b2be7d24d52c779
b2blosangeles <b2blosangeles@gmail.com>
2017-01-25 18:56:34 -0800

    test

--- jsx/router.jsx
+++ jsx/router.jsx
@@ -23,7 +23,6 @@ class App extends React.Component {
 ReactDOM.render((
    <Router history = {browserHistory}>
       <Route path = "/" component = {App}>
-         <IndexRoute component = {Home} />
          <Route path = "home" component = {Home} />
          <Route path = "about" component = {About} />
          <Route path = "contact" component = {Contact} />
