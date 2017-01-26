c6c02eac41660f0d2f96bcae3b0fb6f42fd7c15b
b2blosangeles <b2blosangeles@gmail.com>
2017-01-25 16:33:34 -0800

    Update react_int.jsx

--- package/react_int.jsx
+++ package/react_int.jsx
@@ -3,7 +3,7 @@
     "filelist":[
         "/js/react/react.min.js",
         "/js/react/react-dom.min.js",
-		"/js/jquery-2.1.3.min.js",
-	    	"/jsx/form.jsx"
+	"/js/jquery-2.1.3.min.js",
+	"/jsx/router.jsx"
       ]
 }
