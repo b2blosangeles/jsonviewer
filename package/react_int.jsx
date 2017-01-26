a23ebd73f647adaf783f251324844fa3e65077a0
b2blosangeles <b2blosangeles@gmail.com>
2017-01-25 18:15:17 -0800

    test

--- package/react_int.jsx
+++ package/react_int.jsx
@@ -1,9 +1,10 @@
 {
     "tpls":[],
     "filelist":[
-        "/js/react/react.min.js",
-        "/js/react/react-dom.min.js",
-	"/js/jquery-2.1.3.min.js",
-	"/jsx/router.jsx"
+		"/js/react/react.min.js",
+		"/js/react/react-dom.min.js",
+		"/js/react/react-router.min.js",
+		"/js/jquery-2.1.3.min.js",
+		"/jsx/router.jsx"
       ]
 }
