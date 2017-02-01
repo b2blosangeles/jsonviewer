5893119feed842a2d33953d192a126fce6b450e8
b2blosangeles <b2blosangeles@gmail.com>
2017-02-01 15:14:30 -0800

    Update router.jsx

--- jsx/router.jsx
+++ jsx/router.jsx
@@ -10,7 +10,7 @@ var Contact = React.createClass({
   render: function() {
       return (
         <div>
-          <h2>GOT QUESTIONS?</h2>
+          <h2>GOT QUESTIONS==>?</h2>
           <p>The easiest thing to do is post on
           our <a href="http://forum.kirupa.com">forums</a>.
           </p>
