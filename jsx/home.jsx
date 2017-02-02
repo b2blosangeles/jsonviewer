f524ae2265f9a512fc47ad1d3015d8c21a59b450
b2blosangeles <b2blosangeles@gmail.com>
2017-02-02 06:53:52 -0800

    test

--- jsx/home.jsx
+++ jsx/home.jsx
@@ -1,4 +1,4 @@
-var Home = React.createClass({
+var Home = React.createclassName({
 	getInitialState: function() {
 		return {};
 	},
@@ -15,9 +15,57 @@ var Home = React.createClass({
 	},	
 	render: function() {
 		return (
+			<div className="container-fluid">
+				<div className="row">
 						<button type="button" className="btn btn-warning pull-right" onClick={this.sendData} >
 											Show Original
 										</button>	
+					<table border="1">
+						<tr>
+							<td width="180"><img src="http://www.qalet.com/images/qalet_main_logo.png" height="36"/></td>
+							<td width="*"><h3> Document viewer ==</h3></td>
+						</tr>	
+					</table>
+					
+					 <form>
+						 <div className="container-fluid form_section">
+							<div className="row">
+								<div className="col-sm-12">
+									<div className="form-group">
+										<button type="button" className="btn btn-warning pull-right" onClick={this.sendData} >
+											Show Original
+										</button>	
+									
+										<label for="comment">
+										  Data Source: (url or text)
+											
+										</label>
+									  
+										<textarea className="form-control" rows="5" name="code"></textarea>
+									</div>	
+
+								</div>
+								<div className="col-sm-12">
+									<label for="sel1">Show data as:</label>
+									<button type="button" className="btn btn-info" onClick={this.sendData}>JSON</button>
+									<button type="button" className="btn btn-info" onClick={this.sendData}>XML</button>
+									<button type="button" className="btn btn-info" onClick={this.sendData}>Markdown</button>
+								 </div>
+							</div>
+						</div>
+					</form>						
+				
+					<div className="doc_renderer" id="doc-renderer"></div>
+					<div className="sample_section">  
+						Sample:<br/>
+						<ul>
+						  <li><a href="JavaScript:void(0)" onClick={this.loadSample(1,7)}>http://docviewer.qalet.com/README.md</a></li>
+						  <li><a href="JavaScript:void(0)" onClick={this.loadSample(2,9)}>http://docviewer.qalet.com/sample/data.json</a></li>
+						  <li><a href="JavaScript:void(0)" onClick={this.loadSample(3,8)}>http://docviewer.qalet.com/sample/data.xml</a></li>
+						</ul>	
+					</div>
+				</div>	
+			</div>
 		  );
 	}
 });
