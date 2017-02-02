c629ca6f9ff658c80ea2f32716fcfaebf6134d2a
b2blosangeles <b2blosangeles@gmail.com>
2017-02-01 22:50:24 -0800

    test

--- jsx/home.jsx
+++ jsx/home.jsx
@@ -12,19 +12,51 @@ handleChange:function(){
 	this.state.value = "123";
     return (
         <div>
-          <h2>HELLO==<span dangerouslySetInnerHTML={{ __html : pp.html }}/> </h2>
-          <p>Cras facilisis urna ornare ex volutpat, et
-          convallis erat elementum. Ut aliquam, ipsum vitae
-          gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-          metus nec massa. Maecenas hendrerit laoreet augue
-          nec molestie. Cum sociis natoque penatibus et magnis
-          dis parturient montes, nascetur ridiculus mus.</p>
-  
-          <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
-			<textarea
-          onChange={this.handleChange}
-          ref="textarea"
-          defaultValue="t" />		  
+			<table>
+				<tr>
+					<td width="180"><img src="http://www.qalet.com/images/qalet_main_logo.png" height="36"></td>
+					<td width="*"><h3> Document viewer</h3></td>
+				</tr>	
+			</table>
+						
+			
+			 <form>
+				 <div class="container-fluid form_section">
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="form-group">
+							<button type="button" class="btn btn-warning pull-right" onClick="sendData('')" style="margin-top:-12px">
+								Show Original
+							</button>	
+							
+							<label for="comment">
+								  Data Source: (url or text)
+									
+							  </label>
+							  
+							  <textarea class="form-control" rows="5" name="code"></textarea>
+							</div>	
+
+						</div>
+						<div class="col-sm-12">
+							<label for="sel1">Show data as:</label>
+							<button type="button" class="btn btn-info" onClick="sendData('json')">JSON</button>
+							<button type="button" class="btn btn-info" onClick="sendData('xml')">XML</button>
+							<button type="button" class="btn btn-info" onClick="sendData('md')">Markdown</button>
+						 </div>
+					</div>
+				</div>
+			</form>
+			
+			<div class="doc_renderer" id="doc-renderer"></div>
+			<div class="sample_section">  
+				Sample:<br/>
+				<ul>
+				  <li><a href="JavaScript:void(0)" onClick="loadSample('http://docviewer.qalet.com/README.md', 'md')">http://docviewer.qalet.com/README.md</a></li>
+				  <li><a href="JavaScript:void(0)" onClick="loadSample('http://docviewer.qalet.com//sample/data.json', 'json')">http://docviewer.qalet.com/sample/data.json</a></li>
+				  <li><a href="JavaScript:void(0)" onClick="loadSample('http://docviewer.qalet.com/sample/data.xml', 'xml')">http://docviewer.qalet.com/sample/data.xml</a></li>
+				</ul>	
+			</div>			  
         </div>
       );
     }
