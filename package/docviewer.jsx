c989664166e8a0039388ba3a8dcf09ef6c21f79c
b2blosangeles <b2blosangeles@gmail.com>
2017-02-01 20:30:21 -0800

    test

--- package/docviewer.jsx
+++ package/docviewer.jsx
@@ -4,7 +4,6 @@
 		"/js/react/react.min.js",
 		"/js/react/react-dom.min.js",
 	    	"/js/react/ReactRouter/2.4.0/ReactRouter.min.js",
-		"/js/jquery-2.1.3.min.js",
 			"/jsx/home.jsx",
 			"/jsx/docviewer/router.jsx"
       ]
