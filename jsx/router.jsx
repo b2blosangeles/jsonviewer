d1db4beee1c6bf3ffe6cf86562616e24b02c0270
b2blosangeles <b2blosangeles@gmail.com>
2017-02-01 17:54:11 -0800

    test

--- jsx/router.jsx
+++ jsx/router.jsx
@@ -13,6 +13,8 @@ var Contact = React.createClass({
           <h2>GOT QUESTIONS==>?</h2>
           <p>The easiest thing to do is post on
           our <a href="http://forum.kirupa.com">forums</a>.
+		  ===
+		  <Stuff/>
           </p>
         </div>
       );
