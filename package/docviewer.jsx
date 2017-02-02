af9daf27115b5e0bd9813069735a28b3d2fafaec
b2blosangeles <b2blosangeles@gmail.com>
2017-02-01 22:05:05 -0800

    test

--- package/docviewer.jsx
+++ package/docviewer.jsx
@@ -3,8 +3,13 @@
     "filelist":[
 		"/js/react/react.min.js",
 		"/js/react/react-dom.min.js",
-	    	"/js/react/ReactRouter/2.4.0/ReactRouter.min.js",
-			"/jsx/home.jsx",
-			"/jsx/docviewer/router.jsx"
+		"/js/react/ReactRouter/2.4.0/ReactRouter.min.js",
+		"/js/jquery-2.1.3.min.js",  
+		"/js/jquery.json-viewer.js",
+		"/js/showdown/showdown.js",
+		"/js/x2js/xml2json.js",
+		"/js/json2.min.js",			
+		"/jsx/home.jsx",
+		"/jsx/docviewer/router.jsx"
       ]
 }
