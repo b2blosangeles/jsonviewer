2f2eade4eb29f2ae7be4b7806ad016cd0cb92374
b2blosangeles <b2blosangeles@gmail.com>
2017-02-01 20:13:14 -0800

    test

--- jsx/router.jsx
+++ jsx/router.jsx
@@ -65,7 +65,6 @@ var App = React.createClass({
   }
 });
 $(function() {
-    console.log( "ready OK!" );
 	ReactDOM.render(
 		<ReactRouter.Router>
 		    <ReactRouter.Route path="/" component={App}>
@@ -76,7 +75,7 @@ $(function() {
 		    </ReactRouter.Route>
 		  </ReactRouter.Router>
 	,
-	 $("#container")[0]
+	 $('body')[0]
 	);	
 	
 });
