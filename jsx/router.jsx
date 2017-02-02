942aafb299e771832b7b13c35dd453da62d3ee31
b2blosangeles <b2blosangeles@gmail.com>
2017-02-01 17:59:17 -0800

    test

--- jsx/router.jsx
+++ jsx/router.jsx
@@ -25,7 +25,7 @@ var Stuff = React.createClass({
   render: function() {
       return (
         <div>
-          <h2>STUFF{this.props.name}</h2>
+          <h2>STUFF{this.props.name+'88'}</h2>
           <p>Mauris sem velit, vehicula eget sodales vitae,
           rhoncus eget sapien:</p>
           <ol>
