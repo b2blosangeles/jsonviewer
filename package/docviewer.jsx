57a044c00ecd26d5fe444add8b5574b83538729d
b2blosangeles <b2blosangeles@gmail.com>
2017-02-02 15:18:29 -0800

    Update docviewer.jsx

--- package/docviewer.jsx
+++ package/docviewer.jsx
@@ -3,7 +3,7 @@
     "filelist":[
 		"/js/react/react.min.js",
 		"/js/react/react-dom.min.js",
-		"/js/react/ReactRouter/3.02/ReactRouter.min.js",
+		"/js/react/ReactRouter/3.0.2/ReactRouter.min.js",
 		"/js/jquery-2.1.3.min.js",  
 		"/js/showdown/showdown.min.js",
 		"/js/x2js/xml2json.min.js",
