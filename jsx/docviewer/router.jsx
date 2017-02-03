f98fdefb3072762e26701b55a16e6affadbc1a50
b2blosangeles <b2blosangeles@gmail.com>
2017-02-02 16:15:20 -0800

    Update router.jsx

--- jsx/docviewer/router.jsx
+++ jsx/docviewer/router.jsx
@@ -9,27 +9,6 @@ var { Router,
       IndexLink,
       Link } = ReactRouter;
 
-
-
-var Stuff = React.createClass({
-  render: function() {
-      return (
-        <div>
-          <h2>STUFF{this.props.name + " ** " +  ((this.props.parent)?this.props.parent.qq:'==')}</h2>
-          <p>Mauris sem velit, vehicula eget sodales vitae,
-          rhoncus eget sapien:</p>
-          <ol>
-            <li>Nulla pulvinar diam</li>
-            <li>Facilisis bibendum</li>
-            <li>Vestibulum vulputate</li>
-            <li>Eget erat</li>
-            <li>Id porttitor</li>
-          </ol>
-        </div>
-      );
-    }
-});
-
 var App = React.createClass({
   render: function() {
     return (
