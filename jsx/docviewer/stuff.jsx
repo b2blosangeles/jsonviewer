8b3f9f57c9f9c742d56383ce5cadae08e82507c3
b2blosangeles <b2blosangeles@gmail.com>
2017-02-02 16:14:57 -0800

    Create stuff.jsx

---
+++ jsx/docviewer/stuff.jsx
@@ -0,0 +1,18 @@
+var Stuff = React.createClass({
+  render: function() {
+      return (
+        <div>
+          <h2>STUFF{this.props.name + " ** " +  ((this.props.parent)?this.props.parent.qq:'==')}</h2>
+          <p>Mauris sem velit, vehicula eget sodales vitae,
+          rhoncus eget sapien:</p>
+          <ol>
+            <li>Nulla pulvinar diam</li>
+            <li>Facilisis bibendum</li>
+            <li>Vestibulum vulputate</li>
+            <li>Eget erat</li>
+            <li>Id porttitor</li>
+          </ol>
+        </div>
+      );
+    }
+});
