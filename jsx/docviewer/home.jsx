b3fa0c5cbd9124532ac91203356edf703e94c58b
b2blosangeles <b2blosangeles@gmail.com>
2017-02-03 17:16:49 -0800

    Update home.jsx

--- jsx/docviewer/home.jsx
+++ jsx/docviewer/home.jsx
@@ -1,3 +1,37 @@
+var Form = React.createClass({
+	render: function() {
+		return (	
+			 <form>
+				 <div className="container-fluid form_section">
+					<div className="row">===
+						<div className="col-sm-12">
+							<div className="form-group">
+								<button type="button" className="btn btn-warning pull-right form_button" onClick={this.sendData('')}>
+									Show Original
+								</button>	
+
+								<label for="comment">
+								  Data Source: (url or text)
+
+								</label>
+
+								<textarea className="form-control" rows="5" name="code"></textarea>
+							</div>	
+
+						</div>
+						<div className="col-sm-12">
+							<label for="sel1">Show data as:</label>
+							<button type="button" className="btn btn-info form_button" onClick={this.sendData('json')}>JSON</button>
+							<button type="button" className="btn btn-info form_button" onClick={this.sendData('xml')}>XML</button>
+							<button type="button" className="btn btn-info form_button" onClick={this.sendData('md')}>Markdown</button>
+						 </div>
+					</div>
+				</div>
+			</form>	
+		);	
+	}
+});
+		
 var Home = React.createClass({
 	getInitialState: function() {
 		return {};
@@ -69,33 +103,7 @@ var Home = React.createClass({
 		return (
 			<div className="container-fluid">
 				<div className="row">
-					 <form>
-						 <div className="container-fluid form_section">
-							<div className="row">
-								<div className="col-sm-12">
-									<div className="form-group">
-										<button type="button" className="btn btn-warning pull-right form_button" onClick={this.sendData('')}>
-											Show Original
-										</button>	
-									
-										<label for="comment">
-										  Data Source: (url or text)
-											
-										</label>
-									  
-										<textarea className="form-control" rows="5" name="code"></textarea>
-									</div>	
-
-								</div>
-								<div className="col-sm-12">
-									<label for="sel1">Show data as:</label>
-									<button type="button" className="btn btn-info form_button" onClick={this.sendData('json')}>JSON</button>
-									<button type="button" className="btn btn-info form_button" onClick={this.sendData('xml')}>XML</button>
-									<button type="button" className="btn btn-info form_button" onClick={this.sendData('md')}>Markdown</button>
-								 </div>
-							</div>
-						</div>
-					</form>						
+					<From/>
 				
 					<div className="doc_renderer" id="doc-renderer"></div>
 					<div className="sample_section">  
