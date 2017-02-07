915f2569cf2de712a514f5848781ec9daebd3416
b2blosangeles <b2blosangeles@gmail.com>
2017-02-07 11:29:33 -0800

    test
    test

--- jsx/docviewer/router_ad.jsx
+++ jsx/docviewer/router_ad.jsx
@@ -12,7 +12,9 @@
 	  render: function() {
 		return (
 		  <div className="container-fluid">
-			<img src="http://www.qalet.com/images/qalet_main_logo.png" height="36"/>
+			<video id="main_video" class="center" controls="" style="" src="http://videorepo.com/api/streaming.js?vid=lKcZ551Ex4w.mp4" width="300">
+			Your browser does not support HTML5 video. 
+			</video>
 		  </div>
 		)
 	  }
