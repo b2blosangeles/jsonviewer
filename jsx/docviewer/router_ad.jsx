ad0e09d304d1e955596842ef86a866e737512878
b2blosangeles <b2blosangeles@gmail.com>
2017-02-07 11:04:53 -0800

    test

--- jsx/docviewer/router_ad.jsx
+++ jsx/docviewer/router_ad.jsx
@@ -35,9 +35,11 @@
 	});
 	$(function() {
 		ReactDOM.render(
-			<video id="main_video" class="center" controls="" style="" src="http://videorepo.com/api/streaming.js?vid=lKcZ551Ex4w.mp4" width="300">
-				Your browser does not support HTML5 video. 
-			</video>
+			<video width="320" height="240" controls>
+			  <source src="http://videorepo.com/api/streaming.js?vid=lKcZ551Ex4w.mp4" type="video/mp4">
+			  <source src="movie.ogg" type="video/ogg">
+			Your browser does not support the video tag.
+			</video>		
 			,
 			$('#qalet_plugin_ad')[0]
 		);	
