1e57aaa84ca10f1fd0562c74c8a92ff73fdebb26
b2blosangeles <b2blosangeles@gmail.com>
2017-02-07 11:10:41 -0800

    test

--- jsx/docviewer/router_ad.jsx
+++ jsx/docviewer/router_ad.jsx
@@ -35,11 +35,7 @@
 	});
 	$(function() {
 		ReactDOM.render(
-			<video width="320" height="240" controls>
-			  <source src="http://videorepo.com/api/streaming.js?vid=lKcZ551Ex4w.mp4" type="video/mp4">
-			  <source src="movie.ogg" type="video/ogg">
-			Your browser does not support the video tag.
-			</video>		
+			<span>test</span>		
 			,
 			$('#qalet_plugin_ad')[0]
 		);	
