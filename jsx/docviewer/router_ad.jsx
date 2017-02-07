cad4c100677e8637748356ef8a7e835735b9f3fc
b2blosangeles <b2blosangeles@gmail.com>
2017-02-07 12:13:05 -0800

    test

--- jsx/docviewer/router_ad.jsx
+++ jsx/docviewer/router_ad.jsx
@@ -13,13 +13,18 @@
 			return {};
 		},
 		componentDidMount : function() {
-			$('.niu').html('---');
+			$('.niu').html(
+				'<video width="320" height="240" controls>'+
+				'  <source src="http://videorepo.com/api/streaming.js?vid=lKcZ551Ex4w.mp4 type="video/mp4">' +
+				'Your browser does not support the video tag.' +
+				'</video>'
+			);
 		},	
 		render: function() {
 			return (
 				<div className="container-fluid niu">
-				http://videorepo.com/api/streaming.js?vid=lKcZ551Ex4w.mp4
-			</div>
+					http://videorepo.com/api/streaming.js?vid=lKcZ551Ex4w.mp4
+				</div>
 		)
 	  }
 	});
