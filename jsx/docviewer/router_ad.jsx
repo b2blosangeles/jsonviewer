8ed4b7b660208b54b6386914384b3623a19bddb9
b2blosangeles <b2blosangeles@gmail.com>
2017-02-07 11:24:54 -0800

    test

--- jsx/docviewer/router_ad.jsx
+++ jsx/docviewer/router_ad.jsx
@@ -8,7 +8,7 @@
 		  IndexLink,
 		  Link } = ReactRouter;
 
-	var Bpp = React.createClass({
+	var App = React.createClass({
 	  render: function() {
 		return (
 		  <div className="container-fluid">
@@ -19,7 +19,7 @@
 	});
 	$(function() {
 		ReactDOM.render(
-			<Bpp/>	
+			<App/>	
 			,
 			$('#qalet_plugin_ad')[0]
 		);	
