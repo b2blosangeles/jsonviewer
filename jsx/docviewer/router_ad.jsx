5e8b5a584d68bbaef8780e8edb07c2646ef68d35
b2blosangeles <b2blosangeles@gmail.com>
2017-02-07 11:00:42 -0800

    test

--- jsx/docviewer/router_ad.jsx
+++ jsx/docviewer/router_ad.jsx
@@ -41,7 +41,7 @@
 		);	
 		
 	});
-}();
+})();
 //		<video id="main_video" class="center" controls="" style="" src="http://videorepo.com/api/streaming.js?vid=lKcZ551Ex4w.mp4" width="300">
 //			Your browser does not support HTML5 video. 
 //		</video>
