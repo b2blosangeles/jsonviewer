46c75ff2210f016c3adaa2767f9ad2b3f3d9059d
b2blosangeles <b2blosangeles@gmail.com>
2017-02-07 12:14:33 -0800

    test

--- jsx/docviewer/router_ad.jsx
+++ jsx/docviewer/router_ad.jsx
@@ -15,7 +15,7 @@
 		componentDidMount : function() {
 			$('.niu').html(
 				'<video width="320" height="240" controls>'+
-				'  <source src="http://videorepo.com/api/streaming.js?vid=lKcZ551Ex4w.mp4 type="video/mp4">' +
+				'  <source src="http://videorepo.com/api/streaming.js?vid=lKcZ551Ex4w.mp4" type="video/mp4">' +
 				'Your browser does not support the video tag.' +
 				'</video>'
 			);
