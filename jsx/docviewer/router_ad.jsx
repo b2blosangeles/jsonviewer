9e04aea3785f3a0dd96acf8baf272f399da45509
b2blosangeles <b2blosangeles@gmail.com>
2017-02-07 11:16:40 -0800

    test

--- jsx/docviewer/router_ad.jsx
+++ jsx/docviewer/router_ad.jsx
@@ -36,8 +36,7 @@
 	$(function() {
 		ReactDOM.render(
 			<span>test
-				<video width="320" height="240" controls>
-				  <source src="http://videorepo.com/api/streaming.js?vid=lKcZ551Ex4w.mp4 type="video/mp4"/>
+				<video width="320" height="240" src="http://videorepo.com/api/streaming.js?vid=lKcZ551Ex4w.mp4 type="video/mp4" controls>
 				Your browser does not support the video tag.
 				</video>			
 			
