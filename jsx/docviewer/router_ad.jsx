b18cbff8bce1b3e62a1857f44124ad9fe9d3aa3c
b2blosangeles <b2blosangeles@gmail.com>
2017-02-07 11:19:48 -0800

    test

--- jsx/docviewer/router_ad.jsx
+++ jsx/docviewer/router_ad.jsx
@@ -12,23 +12,7 @@
 	  render: function() {
 		return (
 		  <div className="container-fluid">
-			<div className="container-fluid">
-				<table width="100%" border="0">
-					<tr>
-						<td width="228">
-							<img src="http://www.qalet.com/images/qalet_main_logo.png" height="36"/>
-						</td>
-						<td width="220" valign="top" align="left"><h3> Document viewer</h3></td>
-						<td width="*" align="right" valign="bottom">					
-						</td>
-						
-					</tr>	
-				</table>		
-			</div> 
-			<div className="container-fluid">
-			  {this.props.children}
-			</div>	
-			<br/><br/><br/><br/><br/><br/>
+			<img src="http://www.qalet.com/images/qalet_main_logo.png" height="36"/>
 		  </div>
 		)
 	  }
