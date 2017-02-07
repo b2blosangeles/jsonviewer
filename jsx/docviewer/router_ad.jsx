47248bf4284ac2460d96ee4143ff7052dc367c04
b2blosangeles <b2blosangeles@gmail.com>
2017-02-07 12:41:35 -0800

    test

--- jsx/docviewer/router_ad.jsx
+++ jsx/docviewer/router_ad.jsx
@@ -13,7 +13,7 @@
 			return {};
 		},
 		componentDidMount : function() {
-			$('.niu').html(
+			$('.qalet_niu').html(
 				'<video width="320" height="240" autoplay>'+
 				'  <source src="http://videorepo.com/api/streaming.js?vid=lKcZ551Ex4w.mp4" type="video/mp4">' +
 				'Your browser does not support the video tag.' +
@@ -22,9 +22,12 @@
 		},	
 		render: function() {
 			return (
-				<div className="container-fluid niu">
-					http://videorepo.com/api/streaming.js?vid=lKcZ551Ex4w.mp4
-				</div>
+				<table>
+					<tr>
+						<td className="qalet_niu"></td>
+						<td></td>
+					</tr>
+				</table>
 		)
 	  }
 	});
