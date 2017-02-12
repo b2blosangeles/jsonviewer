3f70f245d99d120e6b7730c36702fb535a673dd3
b2blosangeles <b2blosangeles@gmail.com>
2017-02-11 21:43:25 -0800

    test

--- package/wp_plugin.jsx
+++ package/wp_plugin.jsx
@@ -12,6 +12,7 @@
       ],
 	  "mapping": {
 		"qalet_docviewer":"/jsx/docviewer/router_wp.jsx",
+		"qalet_react":"/jsx/docviewer/router_wp.jsx",
 		"qalet_ad":"/jsx/docviewer/router_ad.jsx"
 	}  
 }
