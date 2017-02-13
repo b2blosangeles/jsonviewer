153f9a3edadebb9de1ad737a0646af337dbe6aac
b2blosangeles <b2blosangeles@gmail.com>
2017-02-13 15:39:44 -0800

    Update wp_plugin_test.jsx

--- jsx/wp_plugin_test.jsx
+++ jsx/wp_plugin_test.jsx
@@ -1,11 +1,21 @@
 $(document).ready(
 	function() {
-		$('.qaletplugin').hide();
-		var v = $('.qaletplugin').html();
-		v ='********';
-		$('.qaletplugin').html(v);
-		$('.qaletplugin').show();
-	}
+		function parse(v) {
+			var t = v.replace(/(“|”)/ig, '"');
+			return JSON.parse(t);
+		}			
+		var v = $('.qaletplugin'), r={};
+		for (var i = 0; i < v.length; i++) {
+			var o = parse($(v[i]).attr('data'));
+			if (o.module) {
+				r[o.module] = true;
+				var sid = o.module + '_plugin_' + i;
+				$(v[i]).html('<div class="'+sid+'">'+sid+'</div>').show();
+			}
+		}
+		console.log(Object.keys(r));
 
-);
-	
+		$('<link>').appendTo('head').attr({ type: 'text/css', rel: 'stylesheet', href: '/css/your_css_file.css' });
+		$.getScript('http://docviewer.qalet.com/package/wp_plugin.jsx?plus='+Object.keys(r).join(',') );
+	}
+);	
