6f2b595a352c6b8f5b696913704591f59b40e323
b2blosangeles <b2blosangeles@gmail.com>
2017-02-13 07:01:34 -0800

    test

--- jsx/docviewer/router_ad.jsx
+++ jsx/docviewer/router_ad.jsx
@@ -1,12 +1,12 @@
  	
 	$(document).ready(
 		function() {
-			$('#entry-content').hide();
-			var v = $('#entry-content').html();
+			$('.entry-content').hide();
+			var v = $('.entry-content').html();
 			
 			v.replace('[-qalet_docviewer data=”[456,456]”-]', '********');
 			console.log(v);
-			$('#entry-content').html(v).show();
+			$('.entry-content').html(v).show();
 		}
 	
 	);
