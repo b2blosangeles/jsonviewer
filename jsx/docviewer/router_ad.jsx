ae786e7e9b18ccff7aba2c35344160de3d843007
b2blosangeles <b2blosangeles@gmail.com>
2017-02-13 07:23:04 -0800

    test

--- jsx/docviewer/router_ad.jsx
+++ jsx/docviewer/router_ad.jsx
@@ -1,11 +1,11 @@
  	
 	$(document).ready(
 		function() {
-			$('.entry-content').hide();
-			var v = $('.entry-content').html();
+			$('body').hide();
+			var v = $('body').html();
 			v =v.replace(/\[\-(.*)\-\]/ig, '********');
 			console.log(v);
-			$('.entry-content').html(v).show();
+			$('body').html(v).show();
 		}
 	
 	);
