1617c0f101c43eb354aa8fc1559ce767b9e60094
b2blosangeles <b2blosangeles@gmail.com>
2017-02-14 11:46:10 -0800

    Create doc.jsx

---
+++ jsx/react/doc.jsx
@@ -0,0 +1,59 @@
+	var { Router,
+		  Route,
+		  browserHistory,
+		  createMemoryHistory,
+		  hashHistory,
+		  IndexRoute,
+		  IndexLink,
+		  Link } = ReactRouter;
+
+	var Error = React.createClass({
+	  render: function() {
+		  return (
+		<div className="box_error">404 Error! Link does not exist!</div> 
+		  );
+		}
+	});
+
+	var App = React.createClass({
+	  render: function() {
+		return (
+		  <div className="container-fluid">
+			<div className="container-fluid">
+				<table width="100%" className="qalet_table">
+					<tr>
+						<td width="228">
+							<img src="http://www.qalet.com/images/qalet_main_logo.png" height="36"/>
+						</td>
+						<td width="220" valign="top" align="left"><h3> Document viewer</h3></td>
+						<td width="*" align="right" valign="bottom">			
+						</td>
+						
+					</tr>	
+				</table>		
+			</div> 
+			<div className="container-fluid">
+			  <Home/>
+			</div>	
+			<br/><br/><br/><br/><br/><br/>
+		  </div>
+		)
+	  }
+	});
+	$(function() {
+
+			ReactDOM.render(
+				<ReactRouter.Router history={browserHistory}>
+					<ReactRouter.Route path="/int/" component={App}>
+						<IndexRoute component={Home}/>
+						<Route path="home" component={Home} />
+						<Route path="*" component={Home}/>	
+					</ReactRouter.Route>
+				  </ReactRouter.Router>
+				,
+				 $('.'+mapping_data.id)[0]
+			);		
+
+	
+		
+	});
