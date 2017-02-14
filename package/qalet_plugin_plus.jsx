2ca8dae7a9dbc792d8001610210b57f49302e8ec
b2blosangeles <b2blosangeles@gmail.com>
2017-02-13 22:15:27 -0800

    test

--- package/qalet_plugin_plus.jsx
+++ package/qalet_plugin_plus.jsx
@@ -2,7 +2,7 @@
     "tpls":[],
     "filelist":[],
 	  "mapping": {
-		"docviewer":"/jsx/doc/home.jsx,/jsx/doc/form.jsx,/jsx/doc/doc.jsx",
+		"docviewer":" /jsx/doc/home.jsx, /jsx/doc/form.jsx,/jsx/doc/doc.jsx",
 		"video":"/jsx/ad/ad.jsx"
 	}  
 }
