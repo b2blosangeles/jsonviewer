26f7273f53f1cccfb6a3dcf3c96d5386075968fc
b2blosangeles <b2blosangeles@gmail.com>
2017-02-14 10:22:18 -0800

    Update qalet_plugin.js

--- js/qalet_plugin.js
+++ js/qalet_plugin.js
@@ -22,7 +22,12 @@ $(document).ready(
 				$(v[i]).fadeIn( "slow");
 			}
 		};
-		
+		setTimeout(
+			function() {
+				_CALLBACK_();
+				
+			}, 1000	
+		);
 		$('<link>').appendTo('head').attr({ type: 'text/css', rel: 'stylesheet', href: '//qalet.com/css/bootstrap.min.css' });		
 		$('<link>').appendTo('head').attr({ type: 'text/css', rel: 'stylesheet', href: '//docviewer.qalet.com/package/docviewer_wp.min.css' });
 	//	$.getScript('http://docviewer.qalet.com/package/qalet_plugin_plus.jsx?plus='+Object.keys(r).join(',') + '&callback=_QACALLBACK_');
