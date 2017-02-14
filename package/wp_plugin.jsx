ea70feb72bd8680e0706c003ab6d20b825ed678a
b2blosangeles <b2blosangeles@gmail.com>
2017-02-13 18:16:02 -0800

    test

--- package/wp_plugin.jsx
+++ package/wp_plugin.jsx
@@ -4,15 +4,13 @@
 		"/js/react/react.min.js",
 		"/js/react/react-dom.min.js",
 		"/js/react/ReactRouter/3.0.2/ReactRouter.min.js",
-		"/js/jquery-2.1.3.min.js",  
 		"/js/showdown/showdown.min.js",
 		"/js/x2js/xml2json.min.js",
 		"/js/json2.min.js",	
 		"/js/jquery.json-viewer.js"
       ],
 	  "mapping": {
-		"qalet_docviewer":"/jsx/docviewer/router_wp.jsx",
-		"qalet_react":"/jsx/docviewer/router_ad.jsx",
-		"qalet_ad":"/jsx/docviewer/router_ad.jsx"
+		"docviwer":"/jsx/docviewer/router_wp.jsx",
+		"video":"/jsx/docviewer/router_ad.jsx"
 	}  
 }
