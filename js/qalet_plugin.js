f2b9b2a0844978f5b83ddeabaad3d61ae13e08f0
b2blosangeles <b2blosangeles@gmail.com>
2017-02-13 20:34:40 -0800

    test

---
+++ js/qalet_plugin.js
@@ -0,0 +1,30 @@
+var _QACALLBACK_;
+$(document).ready(
+	function() {
+		function parse(v) {
+			var t = v.replace(/(“|”)/ig, '"');
+			return JSON.parse(t);
+		}			
+		var v = $('.qaletplugin'), r={}, f=[];
+		for (var i = 0; i < v.length; i++) {
+			var o = parse($(v[i]).attr('data'));
+			console.log(o);
+			if (o.module) {
+				r[o.module] = true;
+				o.id = o.module + '_plugin_' + i;
+				f[f.length] = o;
+				 $(v[i]).html('<div class="'+o.id+'"></div>').show();
+			}
+		}
+		
+		_QACALLBACK_ = function() {
+			for (var i=0; i<f.length; i++) {
+				_QALET_[f[i].module](f[i]);
+			}
+		};
+
+		$('<link>').appendTo('head').attr({ type: 'text/css', rel: 'stylesheet', href: '//qalet.com/css/bootstrap.min.css' });		
+		$('<link>').appendTo('head').attr({ type: 'text/css', rel: 'stylesheet', href: '//docviewer.qalet.com/package/docviewer_wp.min.css' });
+		$.getScript('http://docviewer.qalet.com/package/wp_plugin.jsx?plus='+Object.keys(r).join(',') + '&callback=_QACALLBACK_');
+	}
+);	
---
+++ package/qalet_plugin.jsx
@@ -0,0 +1,16 @@
+{
+    "tpls":[],
+    "filelist":[
+		"/js/react/react.min.js",
+		"/js/react/react-dom.min.js",
+		"/js/react/ReactRouter/3.0.2/ReactRouter.min.js",
+		"/js/jquery-2.1.3.min.js",
+		"/js/showdown/showdown.min.js",
+		"/js/x2js/xml2json.min.js",
+		"/js/json2.min.js",	
+		"/js/jquery.json-viewer.js",
+		"/jsx/wp_plugin_test.jsx"
+      ],
+	  "mapping": {
+	}  
+}
