5d4ce7581a20fd9b18e95d4c3ee220844408ef52
b2blosangeles <b2blosangeles@gmail.com>
2017-02-13 18:23:26 -0800

    test

--- jsx/wp_plugin_test.jsx
+++ jsx/wp_plugin_test.jsx
@@ -4,19 +4,29 @@ $(document).ready(
 			var t = v.replace(/(“|”)/ig, '"');
 			return JSON.parse(t);
 		}			
-		var v = $('.qaletplugin'), r={};
+		var v = $('.qaletplugin'), r={}, f=[];
 		for (var i = 0; i < v.length; i++) {
 			var o = parse($(v[i]).attr('data'));
 			console.log(o);
 			if (o.module) {
 				r[o.module] = true;
-				var sid = o.module + '_plugin_' + i;
-				$(v[i]).html('<div class="'+sid+'">'+sid+'</div>').show();
+				o.id = o.module + '_plugin_' + i;
+				f[f.length] = o;
+				$(v[i]).html('<div class="'+o.id+'">'+o.id+'</div>').show();
 			}
 		}
-		console.log(Object.keys(r));
-
+		function _QACALLBACK_ {
+			for (var i=0; i<f.length; i++) {
+				_QALET_[f.module](f);
+			}
+		}
+		
 		$('<link>').appendTo('head').attr({ type: 'text/css', rel: 'stylesheet', href: '/css/your_css_file.css' });
-		$.getScript('http://docviewer.qalet.com/package/wp_plugin.jsx?plus='+Object.keys(r).join(',') );
+		$.getScript('http://docviewer.qalet.com/package/wp_plugin.jsx?plus='+Object.keys(r).join(',') + '&callback=_QACALLBACK_');
+		setTimeout(
+			function() {
+				_QACALLBACK_();
+			}, 3000
+		);
 	}
 );	
