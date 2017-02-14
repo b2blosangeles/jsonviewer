59fa866734a3698be2a2ba96b282cb41fdd5c298
b2blosangeles <b2blosangeles@gmail.com>
2017-02-14 05:55:00 -0800

    test

--- js/qalet_plugin.js
+++ js/qalet_plugin.js
@@ -19,7 +19,7 @@ $(document).ready(
 		_QACALLBACK_ = function() {
 			for (var i=0; i<f.length; i++) {
 				_QALET_[f[i].module](f[i]);
-				$(v[i]).html('<div class="'+f[i].id+' container-fluid"></div>').show();
+				$(v[i]).html('<div class="'+f[i].id+' container-fluid"></div>').fadeIn( "slow");
 			}
 		};
 
