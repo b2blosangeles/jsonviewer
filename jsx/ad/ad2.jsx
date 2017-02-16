9e6d9974ff4f960e967b011acc2fe8049708efbc
b2blosangeles <b2blosangeles@gmail.com>
2017-02-16 11:08:42 -0800

    Update ad2.jsx

--- jsx/ad/ad2.jsx
+++ jsx/ad/ad2.jsx
@@ -30,16 +30,11 @@ $(document).ready(
 			},
 			playVideo : function(vid) {
 				return function() {
-				
 					$('.'+mapping_data.id).find('.qalet_video').html(
 						'<video width="320" height="240" controls autoplay>'+
 						'  <source src="http://videorepo.com/api/streaming.js?vid='+vid+'.mp4" type="video/mp4">' +
 						'Your browser does not support the video tag.' +
 						'</video>'
-					);
-					
-					$('.'+mapping_data.id).find('.qalet_video').html(
-						'test====='
 					);				
 				}
 			},		
