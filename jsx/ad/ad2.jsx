bc716f543f6b2dfd59ef8e6b0afc4bb2b61d8fea
b2blosangeles <b2blosangeles@gmail.com>
2017-02-16 08:10:01 -0800

    test

--- jsx/ad/ad2.jsx
+++ jsx/ad/ad2.jsx
@@ -1,11 +1,4 @@
 
-	var Videoitem =  React.createClass({
-		render: function() {
-			return (
-				<span><a href="JavaScript:void(0)" onClick={this.props.parent.playVideo(this.props.item.vid)} >{this.props.item.title}</a>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
-			)
-		}	
-	});
 	var App = React.createClass({
 		/*
 		getInitialState: function() {
@@ -38,7 +31,6 @@
 			return (
 				<div className="qalet_ad_section"><div className="qalet_video"></div>
 					{this.state.list.map((item, index) => (
-						<Videoitem item={item} parent={this}/>
 					))}									
 				</div>
 			)
@@ -52,7 +44,7 @@
 					 $('.'+mapping_data.id)[0]
 				);	
 				
-		},5
+		},500
 		);
 	});
 
