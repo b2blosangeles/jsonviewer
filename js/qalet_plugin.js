7b989d908e8515f1fb2e26891f84803f5938a402
b2blosangeles <b2blosangeles@gmail.com>
2017-02-15 17:56:47 -0800

    Update qalet_plugin.js

--- js/qalet_plugin.js
+++ js/qalet_plugin.js
@@ -2,7 +2,7 @@ var _CALLBACK_ = function() {
 	$(document).ready(
 		function() {
 			function parse(v) {
-				var t = v.replace(/(“|”)/ig, '"').replace(/(”|”)/ig, '"');
+				var t = v.replace(/(“|”)/ig, '"').replace(/(”|”)/ig, '--');
 				return JSON.parse(t);
 			}			
 			var v = $('.qaletplugin'), r={}, f=[];
