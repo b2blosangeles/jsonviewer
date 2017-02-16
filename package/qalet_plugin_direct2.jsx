cb1bf7e0225ce4daddaca849e020f5c14506ab4a
b2blosangeles <b2blosangeles@gmail.com>
2017-02-16 07:41:11 -0800

    Update qalet_plugin_direct2.jsx

--- package/qalet_plugin_direct2.jsx
+++ package/qalet_plugin_direct2.jsx
@@ -8,9 +8,10 @@
 		"/js/showdown/showdown.min.js",
 		"/js/x2js/xml2json.min.js",
 		"/js/json2.min.js",	
-		"/js/jquery.json-viewer.js"
+		"/js/jquery.json-viewer.js",
+		"/js/qalet_plugin.js"
       ],
-	  "mapping": {	
-		"docviewer":" /jsx/react/home.jsx, /jsx/react/form.jsx, /jsx/react/main.jsx"
+	  "mapping": {
+		"docviewer":" /jsx/doc/home.jsx, /jsx/doc/form.jsx,/jsx/doc/doc.jsx"
 	}  
 }
