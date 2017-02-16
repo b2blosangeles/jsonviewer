409e0505df8ae134226a7e074ecff2de7280dd68
b2blosangeles <b2blosangeles@gmail.com>
2017-02-16 09:55:00 -0800

    Update ad.jsx

--- jsx/ad/ad.jsx
+++ jsx/ad/ad.jsx
@@ -1,18 +1,13 @@
+
 	var Videoitem =  React.createClass({
-		/*
 		render: function() {
 			return (
 				<span><a href="JavaScript:void(0)" onClick={this.props.parent.playVideo(this.props.item.vid)} >{this.props.item.title}</a>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
 			)
-		}
-		*/
-		render: function() {
-			return (
-				<span>test</span>
-			)
-		}		
+		}	
 	});
 	var App = React.createClass({
+		
 		getInitialState: function() {
 			var me = this;
 			$.get('http://videorepo.com/api/youtube.js?opt=getAll',
@@ -27,17 +22,23 @@
 		},
 		playVideo : function(vid) {
 			return function() {
+				/*
 				$('.'+mapping_data.id).find('.qalet_video').html(
 					'<video width="320" height="240" controls autoplay>'+
 					'  <source src="http://videorepo.com/api/streaming.js?vid='+vid+'.mp4" type="video/mp4">' +
 					'Your browser does not support the video tag.' +
 					'</video>'
-				);			
+				);
+				*/
+				$('.'+mapping_data.id).find('.qalet_video').html(
+					'test====='
+				);				
 			}
 		},		
 		componentDidMount : function() {
 			
-		},	
+		},
+			
 		render: function() {
 			return (
 				<div className="qalet_ad_section"><div className="qalet_video"></div>
@@ -51,11 +52,12 @@
 	$(function() {
 		setTimeout(function(){
 				ReactDOM.render(
-					<span> test span </span>	
+					<App/>	
 					,
 					 $('.'+mapping_data.id)[0]
 				);	
 				
-		},5
+		},500
 		);
 	});
+
