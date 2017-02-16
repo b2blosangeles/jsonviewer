ceb52fc1fa6ff337d63fb6ef87dfe0684a9f5a2b
b2blosangeles <b2blosangeles@gmail.com>
2017-02-16 10:19:23 -0800

    Update ad2.jsx

--- jsx/ad/ad2.jsx
+++ jsx/ad/ad2.jsx
@@ -1,8 +1,62 @@
 
+	var Videoitem =  React.createClass({
+		render: function() {
+			return (
+				<span><a href="JavaScript:void(0)" onClick={this.props.parent.playVideo(this.props.item.vid)} >{this.props.item.title}</a>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+			)
+		}	
+	});
+	var App = React.createClass({
+		
+		getInitialState: function() {
+			var me = this;
+			$.get('http://videorepo.com/api/youtube.js?opt=getAll',
+			{}, 
+			function (data) {
+				var d = JSON.parse(data); 
+				me.setState({	list: d }, () => {
+						me.playVideo(d[Math.floor(Math.random()*d.length)].vid)();
+					});				
+			},'text');		
+			return {list:[]};
+		},
+		playVideo : function(vid) {
+			return function() {
+				/*
+				$('.'+mapping_data.id).find('.qalet_video').html(
+					'<video width="320" height="240" controls autoplay>'+
+					'  <source src="http://videorepo.com/api/streaming.js?vid='+vid+'.mp4" type="video/mp4">' +
+					'Your browser does not support the video tag.' +
+					'</video>'
+				);
+				*/
+				$('.'+mapping_data.id).find('.qalet_video').html(
+					'test====='
+				);				
+			}
+		},		
+		componentDidMount : function() {
+			
+		},
+			
+		render: function() {
+			return (
+				<div className="qalet_ad_section"><div className="qalet_video"></div>
+					{this.state.list.map((item, index) => (
+						<Videoitem item={item} parent={this}/>
+					))}									
+				</div>
+			)
+		  }
+	});
 	$(function() {
-		ReactDOM.render(
-			<span> test span </span>	
-			,
-			 $('.'+mapping_data.id)[0]
-		);	
+		setTimeout(function(){
+				ReactDOM.render(
+					<span>test niu</span>	
+					,
+					 $('.'+mapping_data.id)[0]
+				);	
+				
+		},500
+		);
 	});
