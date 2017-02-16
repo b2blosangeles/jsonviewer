732c309077ede311a01ac977ba7aaaa5fbd50613
b2blosangeles <b2blosangeles@gmail.com>
2017-02-16 08:07:48 -0800

    test

--- jsx/ad/ad2.jsx
+++ jsx/ad/ad2.jsx
@@ -2,11 +2,12 @@
 	var Videoitem =  React.createClass({
 		render: function() {
 			return (
-				<span>1234</span>
+				<span><a href="JavaScript:void(0)" onClick={this.props.parent.playVideo(this.props.item.vid)} >{this.props.item.title}</a>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
 			)
 		}	
 	});
 	var App = React.createClass({
+		/*
 		getInitialState: function() {
 			var me = this;
 			$.get('http://videorepo.com/api/youtube.js?opt=getAll',
@@ -31,7 +32,8 @@
 		},		
 		componentDidMount : function() {
 			
-		},	
+		},
+		*/	
 		render: function() {
 			return (
 				<div className="qalet_ad_section"><div className="qalet_video"></div>
