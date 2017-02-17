ca2a45d21999e1ba59d1fd27e1862af0c7c6dc47
b2blosangeles <b2blosangeles@gmail.com>
2017-02-16 17:34:31 -0800

    Update main_router.jsx

--- jsx/react/main_router.jsx
+++ jsx/react/main_router.jsx
@@ -25,7 +25,7 @@
 						<td width="228">
 							<img src="http://www.qalet.com/images/qalet_main_logo.png" height="36"/>
 						</td>
-						<td width="220" valign="top" align="left"><h3> Document viewer A</h3></td>
+						<td width="220" valign="top" align="left"><h3> Document viewer</h3></td>
 						<td width="*" align="right" valign="bottom">			
 						</td>
 						
