67e7526e7f8181dcfa17e224dc6ad200069ffda4
b2blosangeles <b2blosangeles@gmail.com>
2017-02-16 17:33:00 -0800

    Update main_router.jsx

--- jsx/react/main_router.jsx
+++ jsx/react/main_router.jsx
@@ -1,93 +1,59 @@
-			
-	var Home = React.createClass({
-		getInitialState: function() {
-			this.onClickLoadSample  = this.loadSample.bind(this);
-			return {};
-		},
-		handleChange:function(){
-			console.log(new Date());
-		},
-		hh : function() {
-			console.log('=hh=');
-		},
-		HtmlViwer : function(data, type, target) {
-			target.removeClass("box_error");
-			if (type == 'md') {
-				var converter = new showdown.Converter();
-				target.html(converter.makeHtml(data));
-			}	
-			if (type == 'json') {
-				var options = {
-				  collapsed: false,
-				  withQuotes: false
-				};
-				try{
-					target.jsonViewer(JSON.parse(data), options);  
-				} catch	(err) {
-					target.addClass("box_error").html('Error! ' + err.message);	   
-					}
-				 
-			}
-			if (type == 'xml') {
-				var x2js = new X2JS();
-				var jsonObj = x2js.xml_str2json( data );
-				console.log(jsonObj);
-				var options = {
-				  collapsed: false,
-				  withQuotes: true
-				};
-				target.jsonViewer(jsonObj, options);  	
-			}
-			if (type == '') {
-				target.html(data.replace(/\n/ig, '<br/>'));  	
-			}			
-		},	
-		sendData:function(type) {
-			var me = this;
-			return function() {
-				var code  =   $('form').find("[name='code']").val();
-				
-				var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+	var { Router,
+		  Route,
+		  browserHistory,
+		  createMemoryHistory,
+		  hashHistory,
+		  IndexRoute,
+		  IndexLink,
+		  Link } = ReactRouter;
 
-				if (pattern.test(code)) {
-					$.get('http://docviewer.qalet.com/api/dataChannel.js',
-					{url:code}, 
-					function (data) {
-						me.HtmlViwer(data, type, $('#doc-renderer')); 
-						},'text'
-					);
-					return true;
-				} else {  
-					me.HtmlViwer(code, type, $('#doc-renderer'));  
-				}		
-			}
-		},
-		loadSample:function(url,type) {
-			var me = this;
-			return function() {
-				me.hh();
-				$('form').find("[name='code']").val(url);
-				me.sendData(type);
-			}
-			
-		},	
-		render: function() {
-			return (
-				<div className="container-fluid">
-					<div className="row">
-						<Form parent={this}/>
-					
-						<div className="doc_renderer" id="doc-renderer"></div>
-						<div className="sample_section">  
-							Sample:<br/>
-							<ul>
-							  <li><a href="JavaScript:void(0)" onClick={this.onClickLoadSample('http://docviewer.qalet.com/README.md','md')}>http://docviewer.qalet.com/README.md</a></li>
-							  <li><a href="JavaScript:void(0)" onClick={this.loadSample('http://docviewer.qalet.com/sample/data.json','json')}>http://docviewer.qalet.com/sample/data.json</a></li>
-							  <li><a href="JavaScript:void(0)" onClick={this.loadSample('http://docviewer.qalet.com/sample/data.xml','xml')}>http://docviewer.qalet.com/sample/data.xml</a></li>
-							</ul>	
-						</div>
-					</div>	
-				</div>
-			  );
+	var Error = React.createClass({
+	  render: function() {
+		  return (
+		<div className="box_error">404 Error! Link does not exist!</div> 
+		  );
 		}
+	});
+
+	var App = React.createClass({
+	  render: function() {
+		return (
+		  <div className="container-fluid">
+			<div className="container-fluid">
+				<table width="100%" className="qalet_table">
+					<tr>
+						<td width="228">
+							<img src="http://www.qalet.com/images/qalet_main_logo.png" height="36"/>
+						</td>
+						<td width="220" valign="top" align="left"><h3> Document viewer A</h3></td>
+						<td width="*" align="right" valign="bottom">			
+						</td>
+						
+					</tr>	
+				</table>		
+			</div> 
+			<div className="container-fluid">
+			  <Home/>
+			</div>	
+			<br/><br/><br/><br/><br/><br/>
+		  </div>
+		)
+	  }
+	});
+	$(function() {
+
+			ReactDOM.render(
+				<ReactRouter.Router history={browserHistory}>
+					<ReactRouter.Route path="/" component={App}>
+						<IndexRoute component={Home}/>
+						<Route path="home" component={Home} />
+						<Route path="*" component={Home}/>	
+					</ReactRouter.Route>
+				  </ReactRouter.Router>
+				,
+				 $('.'+mapping_data.id)[0]
+			);		
+
+	
+		
 	});
