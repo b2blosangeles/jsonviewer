38e2514c31bb13785d8c3115e4ae8367cfc69b1d
b2blosangeles <b2blosangeles@gmail.com>
2017-02-18 23:04:50 -0800

    test

--- package/qalet_giant_plugin_direct.jsx
+++ package/qalet_giant_plugin_direct.jsx
@@ -1,15 +1,14 @@
 {
     "tpls":[],
     "filelist":[
-
+		"/js/json2.min.js",
 		"/js/jquery-2.1.3.min.js",
-		
 		"/js/showdown/showdown.min.js",
 		"/js/x2js/xml2json.min.js",		
-		
-		"/js/json2.min.js",	
 		"/js/jquery.json-viewer.js",
+		
 		"/js/qalet_plugin.js",
+		
 		"/js/react/react.min.js",
 		"/js/react/react-dom.min.js",
 		"/js/react/ReactRouter/3.0.2/ReactRouter.min.js"		
