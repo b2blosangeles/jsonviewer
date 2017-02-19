7b2b9801d3dedfe642532be0182ccbc2cf1bbd11
b2blosangeles <b2blosangeles@gmail.com>
2017-02-18 19:22:06 -0800

    test

---
+++ jsx/giant_plugin/home.jsx
@@ -0,0 +1,9 @@
+ReactDOM.render(
+	<div className="container-fluid">
+		<div className="row">
+			test
+		</div>	
+	</div>
+	,
+	 $('.'+mapping_data.id)[0]
+);
--- package/qalet_plugin_plus.jsx
+++ package/qalet_plugin_plus.jsx
@@ -3,6 +3,7 @@
     "filelist":[],
 	  "mapping": {
 		"docviewer":" /jsx/doc/home.jsx, /jsx/doc/form.jsx,/jsx/doc/doc.jsx",
+		"qa_header":"/jsx/doc/doc.jsx",
 		"video":"/jsx/ad/ad.jsx"
 	}  
 }
