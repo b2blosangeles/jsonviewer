7cb6a05b1a61f91d601519706e360b27445b241b
b2blosangeles <b2blosangeles@gmail.com>
2017-02-18 20:12:46 -0800

    test

--- jsx/giant_plugin/body_section.jsx
+++ jsx/giant_plugin/body_section.jsx
@@ -2,15 +2,11 @@ var Body_section = React.createClass({
   render: function() {
 	return (
 		<div className="container-fluid">
-			<table width="100%" className="qalet_table">
+			<table width="100%" className="qalet_table" style={border:'1px solid red'}>
 				<tr>
-					<td width="228">
-						<img src="http://www.qalet.com/images/qalet_main_logo.png" height="36"/>
+					<td width="100%">
+						test
 					</td>
-						<td width="220" valign="top" align="left"><h3>Sample plugin</h3></td>
-						<td width="*" align="right" valign="bottom">			
-					</td>
-					
 				</tr>	
 			</table>		
 		</div> 
