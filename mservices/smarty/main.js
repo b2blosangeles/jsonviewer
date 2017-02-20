b314491dad6d348e6b72487b59de6e24d30748a9
b2blosangeles <b2blosangeles@gmail.com>
2017-02-19 22:48:39 -0800

    test

--- mservices/smarty/main.js
+++ mservices/smarty/main.js
@@ -1,20 +1,23 @@
-$($('.'+mapping_data.id)[0]).html(_TPL_['tpl/smarty/home.html']);
-    var data = {
-        greeting: 'Hi, there are some JScript books you may find interesting:',
-        books : [
-            {
-                title  : 'JavaScript: The Definitive Guide',
-                author : 'David Flanagan',
-                price  : '31.18'
-            },
-            {
-                title  : 'Murach JavaScript and DOM Scripting',
-                author : 'Ray Harris',
-            },
-            {
-                title  : 'Head First JavaScript',
-                author : 'Michael Morrison',
-                price  : '29.54'
-            }
-        ]
-    };
+var data = {
+	greeting: 'Hi, there are some JScript books you may find interesting:',
+	books : [
+		{
+			title  : 'JavaScript: The Definitive Guide',
+			author : 'David Flanagan',
+			price  : '31.18'
+		},
+		{
+			title  : 'Murach JavaScript and DOM Scripting',
+			author : 'Ray Harris',
+		},
+		{
+			title  : 'Head First JavaScript',
+			author : 'Michael Morrison',
+			price  : '29.54'
+		}
+	]
+};
+var tpl = new jSmart(_TPL_['tpl/smarty/home.html']);
+var res = tpl.fetch( data );
+
+$($('.'+mapping_data.id)[0]).html(res);
