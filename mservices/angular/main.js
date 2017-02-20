7c9a7e975d81ec00de0605c49f7e26d21b9d6e21
b2blosangeles <b2blosangeles@gmail.com>
2017-02-20 09:54:23 -0800

    test

--- mservices/angular/main.js
+++ mservices/angular/main.js
@@ -1,28 +1,16 @@
-/*
-angular.element(function() {
-  angular.bootstrap($('.'+mapping_data.id)[0], ['myApp_'+mapping_data.id]);
-});
-$($('.'+mapping_data.id)[0]).attr("ng-controller", "MyController").html(_TPL_['tpl/angularjs/home.html']);
-angular.module('myApp_'+mapping_data.id, [])
-	  .controller('MyController', ['$scope', function ($scope) {
-		$scope.greetMe = mapping_data;
-	  }
-	  
-]);
-*/
-
-var mapp;
+//var mapp;
 angular.element(function() {  
 //	angular.bootstrap($('.'+mapping_data.id)[0], ['mapp']);
 });
 
 $('.'+mapping_data.id).attr("ng-controller", "MyController").html(_TPL_['tpl/angularjs/home.html']);
-var app = angular.module('mapp', []).controller('MyController', ['$scope', function ($scope) {
+var app = angular.module('app', []).controller('MyController', ['$scope', function ($scope) {
 		$scope.greetMe = mapping_data;
-		angular.bootstrap($('.'+mapping_data.id)[0], ['mapp']);
+		
 	  }
 	  
 ]);
+angular.bootstrap($('.'+mapping_data.id)[0], ['app']);
 
 app.directive('topSection', function() {
     return {
