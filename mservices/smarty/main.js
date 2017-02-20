0ac78e5902f64d5e9276c57cb53a0be1306918bb
b2blosangeles <b2blosangeles@gmail.com>
2017-02-19 22:43:32 -0800

    test

--- mservices/smarty/main.js
+++ mservices/smarty/main.js
@@ -1,1 +1,20 @@
-$($('.'+mapping_data.id)[0]).html('smarty');
+$($('.'+mapping_data.id)[0]).html(_TPL_['tpl/smarty/home.html']);
+    var data = {
+        greeting: 'Hi, there are some JScript books you may find interesting:',
+        books : [
+            {
+                title  : 'JavaScript: The Definitive Guide',
+                author : 'David Flanagan',
+                price  : '31.18'
+            },
+            {
+                title  : 'Murach JavaScript and DOM Scripting',
+                author : 'Ray Harris',
+            },
+            {
+                title  : 'Head First JavaScript',
+                author : 'Michael Morrison',
+                price  : '29.54'
+            }
+        ]
+    };
