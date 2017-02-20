10d78206538501f7784d41a8a1eaf8086db9fca7
b2blosangeles <b2blosangeles@gmail.com>
2017-02-20 07:41:55 -0800

    test

--- mservices/angular/main.js
+++ mservices/angular/main.js
@@ -1,20 +1,20 @@
-var m = mapping_data;
+var mm = mapping_data;
 (function(mapping_data) {
 	
-console.log('====7->');
-	var vm = mapping_data;	
-	console.log(vm);
+console.log('====5->');
+			  console.log(mapping_data);	
+	
 	$($('.'+mapping_data.id)[0]).attr("ng-controller", "MyController").html(_TPL_['tpl/angularjs/home.html']);
 	  
 	  angular.module('myApp', [])
 		  .controller('MyController', ['$scope', function ($scope) {
 			  console.log('mapping_data--5->');
-			  console.log(vm);
+			  console.log(mapping_data);
 			$scope.greetMe = mapping_data;
 		  }]);
 
 		angular.element(function() {
 		  angular.bootstrap($('.'+mapping_data.id)[0], ['myApp']);
 		});	
-})(m);
+})(mm);
 	
