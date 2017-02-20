27769ce5b5b701087174ac25607a104b9658b73d
b2blosangeles <b2blosangeles@gmail.com>
2017-02-20 06:46:20 -0800

    test

--- mservices/angular/main.js
+++ mservices/angular/main.js
@@ -3,7 +3,7 @@
 	  var m = mapping_data;
 	  angular.module('myApp', [])
 		  .controller('MyController', ['$scope', function ($scope) {
-			  console.log('mapping_data--->');
+			  console.log('mapping_data-->->');
 			  console.log(m);
 			$scope.greetMe = mapping_data;
 		  }]);
