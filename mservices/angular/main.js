2dea6891d00b0fb2185bc5e923174d5ba58ac891
b2blosangeles <b2blosangeles@gmail.com>
2017-02-20 08:51:15 -0800

    test

--- mservices/angular/main.js
+++ mservices/angular/main.js
@@ -4,6 +4,7 @@ angular.element(function() {
   
 });
 */
+console.log('myApp');
 var myApp;
 $($('.'+mapping_data.id)[0]).attr("ng-controller", "MyController").html(_TPL_['tpl/angularjs/home.html']);
 angular.module('myApp', [])
