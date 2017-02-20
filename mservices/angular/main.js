9e5dc299be61e1a99ac8b2166387162ff13a61c4
b2blosangeles <b2blosangeles@gmail.com>
2017-02-20 10:44:35 -0800

    test

--- mservices/angular/main.js
+++ mservices/angular/main.js
@@ -2,10 +2,7 @@ angular.element(function() {
 	$('.'+mapping_data.id).attr("ng-controller", "rootController").html(_TPL_['tpl/angularjs/home.html']);
 	angular.bootstrap($('.'+mapping_data.id)[0], ['app_'+mapping_data.id]);	 
 });
-var app = angular.module('app_'+mapping_data.id, [
-	'ngCookies',
-	'ngRoute'
-]);
+var app = angular.module('app_'+mapping_data.id, []);
 app.controller('rootController', ['$scope', function ($scope) {
 		$scope.greetMe = mapping_data;
 	
