df610394ef53faf53c6fa75f22b2e9105214997f
b2blosangeles <b2blosangeles@gmail.com>
2017-02-20 10:38:42 -0800

    test

--- mservices/angular/main.js
+++ mservices/angular/main.js
@@ -6,10 +6,10 @@ var app = angular.module('app_'+mapping_data.id, [
 	'ngCookies',
 	'ngRoute'
 ]);
-app.controller('rootController', ['$scope', function ($rootScope, $scope, $location, $http, $cookies, $timeout, $sce) {
+app.controller('rootController', function ($scope) {
 		$scope.greetMe = mapping_data;
 	
 	  }
 	  
-]);
+);
 
