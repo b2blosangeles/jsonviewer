dc44d03fa45315ad6ccdefe5819fffa7f9ae935a
b2blosangeles <b2blosangeles@gmail.com>
2017-02-20 10:01:34 -0800

    test

--- mservices/angular/main.js
+++ mservices/angular/main.js
@@ -1,12 +1,12 @@
 var app = angular.module('app', []).controller('MyController', ['$scope', function ($scope) {
 		$scope.greetMe = mapping_data;
-		
+	$('.'+mapping_data.id).attr("ng-controller", "MyController").html(_TPL_['tpl/angularjs/home.html']);
+	angular.bootstrap($('.'+mapping_data.id)[0], ['app']);		
 	  }
 	  
 ]);
 //angular.element(function() { 
-	$('.'+mapping_data.id).attr("ng-controller", "MyController").html(_TPL_['tpl/angularjs/home.html']);
-	angular.bootstrap($('.'+mapping_data.id)[0], ['app']);
+
 //});
 app.directive('topSection', function() {
     return {
