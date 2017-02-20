df28afea4575618ebf75149ed3ff57e1854f914e
b2blosangeles <b2blosangeles@gmail.com>
2017-02-20 09:57:02 -0800

    test

--- mservices/angular/main.js
+++ mservices/angular/main.js
@@ -1,27 +1,16 @@
-//var mapp;
-angular.element(function() {  
-//	angular.bootstrap($('.'+mapping_data.id)[0], ['mapp']);
-});
-
-
 var app = angular.module('app', []).controller('MyController', ['$scope', function ($scope) {
 		$scope.greetMe = mapping_data;
 		
 	  }
 	  
 ]);
-
-$('.'+mapping_data.id).attr("ng-controller", "MyController").html(_TPL_['tpl/angularjs/home.html']);
-angular.bootstrap($('.'+mapping_data.id)[0], ['app']);
-
+angular.element(function() { 
+	$('.'+mapping_data.id).attr("ng-controller", "MyController").html(_TPL_['tpl/angularjs/home.html']);
+	angular.bootstrap($('.'+mapping_data.id)[0], ['app']);
+});
 app.directive('topSection', function() {
     return {
 	restrict:'E',    
         template:_TPL_['tpl/angularjs/module.html']
     };
 }); 
-
-
-
-
-// angular.bootstrap($('.'+mapping_data.id)[0], ['mapp']);
