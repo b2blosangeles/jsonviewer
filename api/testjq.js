51b5b14055682a24603599ee327ae7e9bd81c596
b2blosangeles <b2blosangeles@gmail.com>
2017-02-21 08:03:46 -0800

    test

--- api/testjq.js
+++ api/testjq.js
@@ -2,10 +2,10 @@ var url = //(req.query.md)?req.query.md:req.post.md;
 //url = 
 'http://google.com';
 if (url) {
-	// var jqdom = require(env.space_path + '/api/jqdom/node_modules/jqdom');
+	var jqdom = require(env.space_path + '/api/jqdom/node_modules/jqdom');
 	pkg.request(url, function (err, resp, body) {
-		//var $ = jqdom(body);
-		res.send(url);
+		var $ = jqdom(body);
+		res.send(body);
 		// res.send($('title').text());
 	});
 } else {
