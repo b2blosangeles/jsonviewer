8332f41845a0d683d2f1671299c80cd864b69c16
b2blosangeles <b2blosangeles@gmail.com>
2017-02-21 08:04:51 -0800

    test

--- api/testjq.js
+++ api/testjq.js
@@ -2,7 +2,7 @@ var url = //(req.query.md)?req.query.md:req.post.md;
 //url = 
 'http://google.com';
 if (url) {
-	var jqdom = require(env.space_path + '/api/jqdom/node_modules/jqdom');
+	var jqdom = require(env.space_path + '/api/pkg/jqdom/node_modules/jqdom');
 	pkg.request(url, function (err, resp, body) {
 		var $ = jqdom(body);
 		res.send(body);
