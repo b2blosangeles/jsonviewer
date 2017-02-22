f269767de446fa5431ff2eec2af227d1c2ec36e6
b2blosangeles <b2blosangeles@gmail.com>
2017-02-22 14:03:35 -0800

    Update main.js

--- mservices/angular/main.js
+++ mservices/angular/main.js
@@ -9,4 +9,12 @@ var app = angular.module('app_'+mapping_data.id, [
 app.controller('rootController', function ($rootScope, $scope, $location, $http, $cookies, $timeout, $sce) {
 	$scope.greetMe = mapping_data;
 	
+	$scope.data = {
+	    availableOptions: [
+	      {id: '1', name: 'Option A'},
+	      {id: '2', name: 'Option B'},
+	      {id: '3', name: 'Option C'}
+	    ],
+	    selectedOption: {id: '3', name: 'Option C'} //This sets the default value of the select in the ui
+	 };
 });
