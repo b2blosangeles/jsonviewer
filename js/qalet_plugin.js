33f19c1ff0a393f87c7f3ddeb26c3a20bbe07237
b2blosangeles <b2blosangeles@gmail.com>
2017-02-26 18:58:03 -0800

    test

--- js/qalet_plugin.js
+++ js/qalet_plugin.js
@@ -38,8 +38,8 @@ var _CALLBACK_ = function() {
 											UIQALET.css.ruleSelect(v.stylesheet,'.'+o.id);
 											
 										//	console.log(jsmarty.render(tpl, {}));
-											jSmart.left_delimiter = '[';
-											jSmart.right_delimiter = ']';										
+											jSmart->left_delimiter = '[';
+											jSmart->right_delimiter = ']';										
 											var tpl = new jSmart(UIQALET.css.stringify(v));
 
 											
@@ -50,7 +50,7 @@ var _CALLBACK_ = function() {
 										}
 										$('.'+o.id).show();									
 									});										
-								}, 4000
+								}, 1000
 							
 							);		
 						})(o);
