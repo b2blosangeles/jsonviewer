c7b7abb5e3b0ea183171882b6366fad6f64022dd
b2blosangeles <b2blosangeles@gmail.com>
2017-02-26 16:24:53 -0800

    Update css.js

--- js/UIQALET/css.js
+++ js/UIQALET/css.js
@@ -1,6 +1,19 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 if (!window.UIQALET) window.UIQALET = {};
 window.UIQALET.css = require('css');
+window.UIQALET.css.ruleSelect = function(rule, code) {
+	var c = (code[0] == '.')?code:('#' + code);
+	if (rule.selectors) {
+		 for (s in rule.selectors) {
+			rule.selectors[s] = c + ' ' + rule.selectors[s];
+
+		 }
+	} else {
+		for (r in rule.rules) {
+			this.ruleSelect(rule.rules[r], code);
+		}
+	}
+};
 },{"css":3}],2:[function(require,module,exports){
 (function (process,__filename){
 /** vim: et:ts=4:sw=4:sts=4
