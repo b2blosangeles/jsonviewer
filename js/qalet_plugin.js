88e001189967e88eb184a1524b7c262e8463224f
b2blosangeles <b2blosangeles@gmail.com>
2017-02-26 16:41:54 -0800

    Update qalet_plugin.js

--- js/qalet_plugin.js
+++ js/qalet_plugin.js
@@ -26,7 +26,7 @@ var _CALLBACK_ = function() {
 							}
 							$.get(URL(o.css), function( data ) { 
 								try {
-									var v = UIQALET.css.parse(data);
+									var v = UIQALET.css.parse(data.replace(/\}([\;|\s]*)/g.'} '));
 									var s = UIQALET.css.ruleSelect(v.stylesheet,o.id);
 									console.log(s);
 									$('head').append('<style>'+s+'</style>');
